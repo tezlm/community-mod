1b04fca0dff2d1967b7395ed2b0a921532075870
NiChrosia <nichrosian@gmail.com>
2021-07-07 12:03:50 -0700

    random uuids

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,4 +1,22 @@
-var uuid = Core.settings.put("uuid", "oh no");
+function random(array) {
+  const index = Math.round(Math.random() * array.length);
+  return array[index];
+}
+
+function multipleRandom(array, amount) {
+  let randoms = [];
+  for (i = 0; i < amount; i++) {
+    randoms = randoms.concat(random(array));
+  }
+  return randoms;
+}
+
+Events.on(ClientPreConnectEvent, () => {
+  const result = multipleRandom("0123456789".split(""), 8).join("");
+  const uuid = Base64Coder.encodeString(result);
+  Core.settings.put("uuid", uuid);
+});
+
 require("hmmvus");
 require("duos");
 require("speedreigner");
