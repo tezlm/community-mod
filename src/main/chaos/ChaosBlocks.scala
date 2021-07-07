cb6dc767b989f31e797e3f40f6d5cf93e7f1e0ba
NiChrosia <nichrosian@gmail.com>
2021-07-07 14:24:17 -0700

    fix indentation

--- src/main/chaos/ChaosBlocks.scala
+++ src/main/chaos/ChaosBlocks.scala
@@ -4,9 +4,9 @@ import mindustry.ctype.ContentList
 import mindustry.world.Block
 
 class ChaosBlocks extends ContentList {
-  var conveyor: Option[Block] = None
+    var conveyor: Option[Block] = None
 
-  override def load(): Unit = {
-    conveyor = Some(new Block("chaotic-conveyor"))
-  }
+    override def load(): Unit = {
+        conveyor = Some(new Block("chaotic-conveyor"))
+    }
 }
