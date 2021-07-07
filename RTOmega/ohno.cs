bbf2ae095ef9b8e82e74ad1b8e89f007fad3e10e
RebornTrack970 <62565267+RebornTrack970@users.noreply.github.com>
2021-07-07 16:02:45 +0300

    cs

---
+++ RTOmega/ohno.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ohno : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.log("ohno");
+    }
+}
