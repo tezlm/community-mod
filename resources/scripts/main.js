adc428feeabf20994b855a2f18fb1157b82660de
sample-text-here <56274831+sample-text-here@users.noreply.github.com>
2021-07-07 12:33:00 -0700

    tabs better

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,20 +1,20 @@
 function random(array) {
-  const index = Math.round(Math.random() * array.length);
-  return array[index];
+	const index = Math.round(Math.random() * array.length);
+	return array[index];
 }
 
 function multipleRandom(array, amount) {
-  let randoms = [];
-  for (i = 0; i < amount; i++) {
-    randoms = randoms.concat(random(array));
-  }
-  return randoms;
+	let randoms = [];
+	for (i = 0; i < amount; i++) {
+		randoms = randoms.concat(random(array));
+	}
+	return randoms;
 }
 
 Events.on(ClientPreConnectEvent, () => {
-  const result = multipleRandom("0123456789".split(""), 8).join("");
-  const uuid = Base64Coder.encodeString(result);
-  Core.settings.put("uuid", uuid);
+	const result = multipleRandom("0123456789".split(""), 8).join("");
+ 	const uuid = Base64Coder.encodeString(result);
+	Core.settings.put("uuid", uuid);
 });
 
 require("hmmvus");
