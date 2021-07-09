1e30e59cb00de98b41cf1b1e33f2efde44ae2f89
CancerGuy <70016211+AzhurniyBaklan@users.noreply.github.com>
2021-07-09 14:41:45 +0500

    Update speedreigner.js

--- src/main/resources/scripts/speedreigner.js
+++ src/main/resources/scripts/speedreigner.js
@@ -1,7 +1,7 @@
 //crab's own hell
 //make the reigh fast af doe
-UnitTypes.reign.speed = 5
-UnitTypes.reign.rotateSpeed = 5
+UnitTypes.reign.speed = 15
+UnitTypes.reign.rotateSpeed = 8
 //make the atrax a motorboat
 UnitTypes.atrax.legCount = 10
 UnitTypes.atrax.speed = 2
