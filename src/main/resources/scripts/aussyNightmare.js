09eea1642df785050e97044cf4766b9e26390f35
JrTRinny <85090668+JrTRinny@users.noreply.github.com>
2021-07-09 13:49:43 +0700

    A Nightmare for those aussies
    spiders.

---
+++ src/main/resources/scripts/aussyNightmare.js
@@ -0,0 +1,10 @@
+//this is Aussies nightmare
+//makes Toxopid goes WOOOP
+UnitTypes.toxopid.speed = 8
+UnitTypes.toxopid.legSpeed = 12
+
+//Arkyid too
+UnitTypes.toxopid.speed = 4
+UnitTypes.toxopid.legSpeed = 6
+
+//Will add more if I want to LOL
