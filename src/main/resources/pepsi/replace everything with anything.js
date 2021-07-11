60d840cb58ad102019fdb845ad4c6e8eeaf0c868
buthed010203 <naguiar010203@gmail.com>
2021-07-10 20:40:33 -0400

    Remove redundant forward slashes

--- src/main/resources/pepsi/replace everything with anything.js
+++ src/main/resources/pepsi/replace everything with anything.js
@@ -1,3 +1,3 @@
 Vars.content.blocks().each(block => block.buildVisibility = BuildVisibility.shown)
 Vars.content.blocks().each(block => block.alwaysReplace = true)
-//this is very cursed to play with, you should play with it//
+//this is very cursed to play with, you should play with it
