11bdb04977482d352b70763cd6813905e8bd8705
Tegas Aziz <49786146+WilloIzCitron@users.noreply.github.com>
2021-07-26 08:57:37 +0700

    make naval fly

---
+++ src/main/resources/scripts/fly-naval.js
@@ -0,0 +1,15 @@
+//assault naval
+
+UnitTypes.omura.flying = true
+UnitTypes.sei.flying = true
+UnitTypes.bryde.flying = true
+UnitTypes.minke.flying = true
+UnitTypes.risso.flying = true
+
+//support naval
+
+UnitTypes.navanax.flying = true
+UnitTypes.aegires.flying = true
+UnitTypes.cyerce.flying = true
+UnitTypes.oxynoe.flying = true
+UnitTypes.retusa.flying = true
